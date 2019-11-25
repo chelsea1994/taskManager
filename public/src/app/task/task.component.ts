66718ebb5113971838da14d81420236cdccc22e0
chelsea1994 <chelsea.reynolds.ok@gmail.com>
2019-11-25 12:22:22 -0600

    added flag feature

--- public/src/app/task/task.component.ts
+++ public/src/app/task/task.component.ts
@@ -53,4 +53,31 @@ completed = false;
     }
   }
 
+  flag(){
+    if(this.task["flag"] == false){
+        this.task["flag"] = true;
+        let observable = this._httpService.editTask(this.task);
+        observable.subscribe(response => {
+            if(response["status"] == false){
+                console.log("There was an issue updating the task object.")
+            }
+            else{
+                console.log("Should have sucessfully updated task's flagged property to TRUE.")
+            }
+        })
+    }
+    else{
+        this.task["flag"] = false;
+        let observable = this._httpService.editTask(this.task);
+        observable.subscribe(response => {
+            if(response["status"] == false){
+                console.log("There was an issue updating the task object.")
+            }
+            else{
+                console.log("Should have sucessfully updated task's flagged property to FALSE.")
+            }
+        })
+    }
+  }
+
 }
